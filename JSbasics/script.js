ac3b30471032472ace4b725e8f188a64f5591e9a
surajkourav-wq <surajkourav094@gmail.com>
2026-04-20 20:51:48 +0530

    Refactor CSS for consistent margin and padding; enhance HTML structure with additional classes for better styling

---
+++ JSbasics/script.js
@@ -0,0 +1,12 @@
+console.log("Hello world");
+console.log("Hello world1234");
+
+let x = 10;
+let y = "10";
+
+console.log(x==y);  // compares value only
+console.log(x===y); // compares value and type both
+
+console.log(typeof x); 
+console.log(typeof y);
+
