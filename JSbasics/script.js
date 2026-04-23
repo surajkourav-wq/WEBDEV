b4058d8e2ecd32fc75a0b1a1f23c3b0effeab3be
surajkourav-wq <surajkourav094@gmail.com>
2026-04-23 23:08:19 +0530

    practiced java script

--- JSbasics/script.js
+++ JSbasics/script.js
@@ -4,6 +4,19 @@ console.log("Hello world1234");
 let x = 10;
 let y = "10";
 
+var a = 10;
+var a = 20;
+var c = 10;
+
+var b = 10;
+
+console.log(a=b);
+console.log(a==b);
+console.log(a===b);
+
+console.log(a==c);
+console.log(a===c);
+
 console.log(x==y);  // compares value only
 console.log(x===y); // compares value and type both
 
