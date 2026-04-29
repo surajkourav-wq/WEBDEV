e39e46fca97f6d8d61d5ddf0ff1ae5803ad21192
surajkourav-wq <surajkourav094@gmail.com>
2026-04-29 19:31:57 +0530

    Add initial HTML structure and JavaScript loops in index.html and script2.js

---
+++ JSbasics/day-2/script2.js
@@ -0,0 +1,33 @@
+// for (var i = 1; i<=10; i++){
+//     console.log(2*i);
+// }
+
+let i = 1;
+while(i<=10){
+    console.log(2*i);
+    i++
+}
+
+let j = 1;
+do{
+    console.log(2*j);
+    j++;
+} while(j<=10);
+
+// for(var j=1; j<=10; j++){
+//     console.log(j, "=", j*j);  
+// }
+
+// let a = [24,58,33,86,96,12,53,23,32,100];
+
+// for(var i = 0; i<10; i++){
+//     console.log(a[i]);  
+// }
+
+// let i = 0;
+
+// while(i<10)
+// {
+//     console.log(a[i]);
+//     i++;
+// }
