837050472d402573413aae55f55805bc8e37cda2
surajkourav-wq <surajkourav094@gmail.com>
2026-04-30 18:27:24 +0530

    Remove outdated HTML and JavaScript files; add new assignment files with loop examples

---
+++ JavaScript/assignment/assignment1/sheet1.js
@@ -0,0 +1,67 @@
+//Print numbers from 1 to 5 using a for loop.
+for(let i = 1; i<6; i++){
+    console.log(i);
+    
+}
+
+//.Print numbers from 1 to 10 using a for loop
+for(let i = 1; i<11; i++){
+    console.log(i);
+    
+}
+
+//Print numbers from 10 to 1 (reverse counting)
+for(let i =10; i>=1;i-- ){
+    
+    console.log(i)
+    
+}
+
+//Print even numbers from 2 to 10
+for(let i = 1; i<=11; i++)
+{
+    if(i%2==0){
+        console.log(i);
+        
+    }
+}
+
+//Print odd numbers from 1 to 9. 
+for(let i = 1; i<10; i++){
+    if(i%2!==0){
+        console.log(i);
+        
+    }
+}
+
+//Print numbers from 1 to 20 with increment of 2
+
+for(let i = 1; i<20; i+=2){
+    
+    console.log(i);
+    
+}
+
+//Print numbers from 5 to 50 with increment of 5. 
+
+for(let i = 5; i<50; i+=5){
+    
+    console.log(i);
+    
+}
+
+
+// Print numbers from 1 to 10 that are divisible by 3. 
+for(let i = 1; i<=10; i++){
+    if(i%3==0){
+        console.log(i);
+        
+    }
+}
+
+//Create a loop and print the square of numbers from 1 to 10
+for(let i = 1; i<11; i++){
+    let square = i*i;
+    console.log(`The square of ${i} is ${square}`);
+    
+}
---
+++ JavaScript/assignment/assignment1/sheet2.js
@@ -0,0 +1,74 @@
+//Write a program to print numbers from 1 to 5using a while loop
+let i = 1;
+while (i<6) {
+    console.log(i);
+    i++;
+}
+
+//Write a program to print numbers from 5 to 1using a while loop.
+let i1 = 5;
+while (i1>=1) {
+    console.log(i1);
+    i1--;
+    
+}
+
+//Write a program to print numbers startingfrom 2 and keep adding 2 while the numberbecomes less than 11.
+let j = 2;
+while (j<11) {
+    console.log(j);
+    j+=2;
+}
+
+//Write a program to calculate the sum ofnumbers from 10 to 5 using a while loop
+
+let num1 = 10;
+let sum = 0;
+
+while (num1 >= 5) {
+    sum += num1;
+    num1--;
+}
+
+console.log("Sum is:", sum);
+
+//Create a while loop and explain the role ofinitialization, condition, and increment in it.
+let k = 1;
+while (k<5)
+     {
+    console.log(k);
+    k++;
+}
+
+//Write a loop and trace its execution step-bystep (initialization → condition → work →update → repeat).
+let j1 = 2;
+while (j1<11) {
+    console.log(j1);
+    j1+=2;
+}
+
+//Write a loop and explain what happens if the variable is not updated inside the loop
+let j2 = 2;
+while (j2<11) {
+    console.log(j2);
+    j2+=2
+    
+}
+
+
+//Create a loop and explain when and why itbecomes an infinite loop.
+let count = 1;
+
+while (count <= 3) {
+    console.log(count);
+    count++;
+    
+}
+
+//Write a loop and explain how the conditioncontrols the stopping of the loop.
+let num = 1;
+while (num<=5) {
+    console.log(num);
+    num++;
+    
+}
