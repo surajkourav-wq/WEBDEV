e9989b4854cc2d0c7b79c1dbc738afb5a6876ef3
surajkourav-wq <surajkourav094@gmail.com>
2026-05-09 19:37:40 +0530

    Add HTML, CSS, and JavaScript files for Age Calculator application

---
+++ JavaScript/Projects/age_calculator/script.js
@@ -0,0 +1,13 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const dob = document.getElementById("DOB").value;
+  const curDate = document.getElementById("CurrentDate").value;
+
+  const Age = Number(curDate.split("-")[0]) - Number(dob.split("-")[0]);
+
+  document.getElementById("DOB").value = "";
+  document.getElementById("CurrentDate").value = "";
+
+  document.getElementById("MyAge").innerText = Age;
+});
