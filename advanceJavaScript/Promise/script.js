49a57579bf9c1051f729641de37a12b1d37f45b5
surajkourav-wq <surajkourav094@gmail.com>
2026-05-23 19:02:01 +0530

    Update document title and modify script tag type in HTML; implement getData function in script.js

--- advanceJavaScript/Promise/script.js
+++ advanceJavaScript/Promise/script.js
@@ -1,3 +1,27 @@
-function getData(value) {
-  
+async function getData(value) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (value === 3) {
+        reject("Query Not Found");
+      } else {
+        console.log("Data Sent", value);
+        resolve("Query Solved");
+      }
+    }, 2000);
+  });
 }
+
+// getData(1)
+//   .then(() => getData(2))
+//   .then(() => getData(3))
+//   .then(() => getData(4))
+//   .then(() => getData(5))
+//   .catch((rej) => console.log(rej));
+
+
+
+await getData(1)
+await getData(2)
+await getData(3)
+await getData(4)
+await getData(5)
