773eae7a4f7399df214a436dbbf13ca790ae68be
surajkourav-wq <surajkourav094@gmail.com>
2026-05-24 13:17:10 +0530

    Add WeatherApp HTML and JavaScript files; implement getWeather and getGeoLocation functions

---
+++ JavaScript/Projects/WeatherApp/script.js
@@ -0,0 +1,37 @@
+const API_KEY = "f4fb80e51cf03e51717fbfa62965a7e9";
+
+async function getWeather() {
+  const cityName = document.getElementById("cityName").value.trim();
+
+  const { Lattitude, Longitude } = await getGeoLocation(cityName);
+
+  //   console.log({ Lattitude, Longitude });
+
+  const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?lat=${Lattitude}&lon=${Longitude}&appid=${API_KEY}`;
+
+  const response = await fetch(WEATHER_API);
+  const data = await response.json();
+
+  //console.log(data);
+
+  const temperature = data.main.temp - 273.15;
+  const feels_like = data.main.feels_like - 273.15;
+
+
+  document.getElementById("Temperature").innerText = temperature.toFixed(0);
+  document.getElementById("feels").innerText = feels_like.toFixed(0);
+}
+
+async function getGeoLocation(city) {
+  const GEO_LOC_API = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
+
+  const response = await fetch(GEO_LOC_API);
+  const data = await response.json();
+
+  //   console.log(data);
+
+  const Lattitude = data[0].lat;
+  const Longitude = data[0].lon;
+
+  return { Lattitude, Longitude };
+}
