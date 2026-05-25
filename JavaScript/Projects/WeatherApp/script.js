e44d66e5efe2f34422f99307fe8de88614e8d304
surajkourav-wq <surajkourav094@gmail.com>
2026-05-25 20:43:03 +0530

    Refactor code structure for improved readability and maintainability

--- JavaScript/Projects/WeatherApp/script.js
+++ JavaScript/Projects/WeatherApp/script.js
@@ -1,4 +1,4 @@
-const API_KEY = "Use Your API key";
+const API_KEY = "f4fb80e51cf03e51717fbfa62965a7e9";
 
 async function getWeather() {
   const cityName = document.getElementById("cityName").value.trim();
@@ -12,10 +12,23 @@ async function getWeather() {
 
   const temperature = data.main.temp - 273.15;
   const feels_like = data.main.feels_like - 273.15;
+  const minTemp = data.main.temp_min - 273.15;
+  const maxTemp = data.main.temp_max - 273.15;
+  const speed = data.wind.speed;
+  const humidity = data.main.humidity;
+  const main = data.weather[0].main;
+  const description = data.weather[0].description;
 
 
   document.getElementById("Temperature").innerText = temperature.toFixed(0);
   document.getElementById("feels").innerText = feels_like.toFixed(0);
+  document.getElementById("min_temp").innerText = minTemp.toFixed(2);
+  document.getElementById("max_temp").innerText = maxTemp.toFixed(1);
+  document.getElementById("speed").innerText = speed;
+  document.getElementById("humid").innerText = humidity;
+  document.getElementById("weather").innerText = main;
+  document.getElementById("type").innerText = description;
+  
 }
 
 async function getGeoLocation(city) {
@@ -29,3 +42,7 @@ async function getGeoLocation(city) {
 
   return { Lattitude, Longitude };
 }
+
+document.addEventListener("keydown", function (event) {
+  const key = event.key;
+});
