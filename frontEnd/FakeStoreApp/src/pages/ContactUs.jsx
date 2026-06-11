bf5ee746c49be7930d5914ece19d12f301dbbaf7
surajkourav-wq <surajkourav094@gmail.com>
2026-06-11 23:13:51 +0530

    feat: update ContactUs, Home, and Product components with proper headings

--- frontEnd/FakeStoreApp/src/pages/ContactUs.jsx
+++ frontEnd/FakeStoreApp/src/pages/ContactUs.jsx
@@ -4,7 +4,7 @@ const ContactUs = () => {
   return (
     <>
       <div>
-        bfhvhjf dvhjsbhjsebfh
+        Contact Us
       </div>
     </>
   )
--- frontEnd/FakeStoreApp/src/pages/Product.jsx
+++ frontEnd/FakeStoreApp/src/pages/Product.jsx
@@ -4,7 +4,7 @@ const Product = () => {
   return (
     <>
     <div>
-      ghfyuhsdfeg
+      Products
     </div>
     </>
   )
